5bae1e74b7ebdcebdd139b709cbcd9e083a4f0cd
Christian Hüning <christian.huening@haw-hamburg.de>
2017-09-07 17:59:29 +0200

    added Test case to make sure label rewriting doesnt rewrite unnecessary stuff

--- k8sclient/k8s_client_test.go
+++ k8sclient/k8s_client_test.go
@@ -25,6 +25,11 @@ func TestGitlabNameToK8sLabel(t *testing.T) {
 	k3, err := GitlabNameToK8sLabel(g3)
 	if err != nil {t.Error(err)}
 	if k3 != "u____.P_uP-Chief" { t.Error("Incorrect replacement of '/'. Res: "+ k3)}
+
+	g4 := "uP-uP-Chief"
+	k4, err := GitlabNameToK8sLabel(g4)
+	if err != nil {t.Error(err)}
+	if k4 != "uP-uP-Chief" { t.Error("Incorrect replacement of nothing")}
 }
 
 func TestK8sLabelToGitlabName(t *testing.T) {
@@ -42,4 +47,9 @@ func TestK8sLabelToGitlabName(t *testing.T) {
 	g3, err := K8sLabelToGitlabName(k3)
 	if err != nil {t.Error(err)}
 	if g3 != "u__.P/uP-Chief" { t.Error("Incorrect replacement of '/'. Res: "+ g3)}
+
+	k4 := "uP-uP-Chief"
+	g4, err := K8sLabelToGitlabName(k4)
+	if err != nil {t.Error(err)}
+	if g4 != "uP-uP-Chief" { t.Error("Incorrect replacement of '/'. Res: "+ g4)}
 }
