e5adf20dc0481470e4dfebb8a36ff0f35100fe01
Christian Hüning <christian.huening@haw-hamburg.de>
2017-12-23 21:34:36 +0100

    updated secret vars

--- graylog/graylog_test.go
+++ graylog/graylog_test.go
@@ -9,9 +9,11 @@ const namespaceName = "test-18F81907-7C88-438E-A648-2069EF5B95B2"
 const userName = "testaccount"
 
 func TestMain(m *testing.M) {
-	os.Setenv("GRAYLOG_BASE_URL", "https://icc-logging.informatik.haw-hamburg.de")
-	os.Setenv("GRAYLOG_ADMIN_USER", "admin")
-	os.Setenv("GRAYLOG_ADMIN_PASSWORD", "ead4ce069ed01360e46aff03c47ec8f36748d558270351aa62d02a72e7d2379a")
+	/*
+	os.Setenv("GRAYLOG_BASE_URL", "")
+	os.Setenv("GRAYLOG_ADMIN_USER", "")
+	os.Setenv("GRAYLOG_ADMIN_PASSWORD", "")
+	*/
 	ret := m.Run()
 	os.Exit(ret)
 }
